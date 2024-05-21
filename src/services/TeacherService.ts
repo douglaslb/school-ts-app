bb11905854dad55b4ee0798118be70dcab0fb992
douglaslb <doouglaslb@gmail.com>
2024-05-20 22:41:52 -0300

    creating teacher service

---
+++ src/services/TeacherService.ts
@@ -0,0 +1,33 @@
+import {
+  Teacher,
+  TeacherCreationType,
+  TeacherUpdateType,
+} from "../domain/Teacher.js";
+import { ConflictError } from "../domain/errors/ConflictError.js";
+import { Service } from "./BaseService.js";
+
+export class TeacherService extends Service {
+  update(id: string, newData: TeacherUpdateType) {
+    const existing = this.findById(id) as Teacher;
+
+    const updated = new Teacher({
+      ...existing.toObject(),
+      ...newData,
+    });
+
+    this.repository.save(updated);
+    return updated;
+  }
+
+  create(creationData: TeacherCreationType) {
+    const existing = this.repository.listBy("document", creationData.document);
+
+    if (existing.length > 0) {
+      throw new ConflictError(creationData.document, this.repository.dbEntity);
+    }
+
+    const newEntity = new Teacher(creationData);
+    this.repository.save(newEntity);
+    return newEntity;
+  }
+}
